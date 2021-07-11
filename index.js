2fd17a79253e8c454f94cf866d07b948fa7c4bdd
Troy <troyakins99@yahoo.com>
2021-07-11 15:57:36 -0500

    commit tree dd3b5428e0f2fdd5a646c717ef18420ea937c6e9

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+// function superbowlWin(array){
+//     array.find((item) => {
+//         if (item.result === 'W'){
+//             console.log(item.year)
+//         } 
+//     })
+// }
+
+function superbowlWin(array){
+   let a =  array.find((item) =>{
+        if (item.result === "W"){return item}});
+        if (a) {
+            return a.year
+        }
+       
+    
+    }
+
